73e964cc0685c495590fe10412a91674e0176bf7
Maciej Bugala <maciej.bugala.kod@gmail.com>
2020-06-01 22:06:47 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/HelloFromClass.java
@@ -0,0 +1,5 @@
+public class HelloFromClass
+{ public static void main(String[] args)
+    {String example = "Hello from my first class!";
+     System.out.println(example);}
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended
+{ public static void main(String[] args)
+    {String przykladowyTekst = "Tralalala";
+     int fajnaLiczba = 3;
+     double fajniejszaLiczba = 3.12;
+     char literkaG = 'G';
+     String nieMamPomysluNaNazwe="Tututu";
+     char tutututu = 'T';
+     System.out.println(przykladowyTekst);
+     System.out.println(fajnaLiczba);
+     System.out.println(fajniejszaLiczba);
+     System.out.println(literkaG);
+     System.out.println(nieMamPomysluNaNazwe);
+     System.out.println(tutututu);}
+}
