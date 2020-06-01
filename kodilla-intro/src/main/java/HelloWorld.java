4fac04fd1642d0c53cb3de09e06493397b33ce82
Maciej Bugala <maciej.bugala.kod@gmail.com>
2020-06-01 21:25:06 +0200

    Zad1

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args)
+    {System.out.println("Hello World!");
+     System.out.println("Maciej Bugała");
+     System.out.println(20);
+     System.out.println('B');}
+}
