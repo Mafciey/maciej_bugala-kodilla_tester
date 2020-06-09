de6bdf2cf90622ad69bf56390e4fe9967d32562d
Maciej Bugala <maciej.bugala.kod@gmail.com>
2020-06-09 11:48:34 +0200

    LeapYearProperUpdate

--- kodilla-intro/src/main/java/Inaczej.java
+++ kodilla-intro/src/main/java/Inaczej.java
@@ -1,19 +1,20 @@
-import java.util.Random;
-public class Inaczej {
-    public int getCountOfRandomNumber(int max) {
-        Random random = new Random();
-        int result = 0;
-        int sum = 0;
-        while (sum < max) {
-            int temp = random.nextInt(10);
-            sum = sum + temp;
-            result++;
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        getCountOfRandomNumber(20);
-        System.out.println(getCountOfRandomNumber(20));
-    }
-}
+//import java.security.spec.RSAOtherPrimeInfo;
+//import java.util.Random;
+//public class Inaczej {
+//    public int getCountOfRandomNumber(int max) {
+//        Random random = new Random();
+//        int result = 0;
+//        int sum = 0;
+//        while (sum < max) {
+//            int temp = random.nextInt(10);
+//            sum = sum + temp;
+//            result++;
+//        }
+//        return result;
+//    }
+//
+//    public static void main(String[] args) {
+//        getCountOfRandomNumber(20);
+//        System.out.println(getCountOfRandomNumber(20));
+//    }
+//}
--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,11 +1,12 @@
 public class LeapYear {
     public static void main(String[] args) {
-        Inaczej inaczej= new Inaczej();
-        inaczej.getCountOfRandomNumber(21);
-        Math.abs();
+        //Inaczej inaczej= new Inaczej();
+        //inaczej.getCountOfRandomNumber(21);
+        //Math.abs();
+        //Math.abs();
 //        Inaczej.getCountOfRandomNumber(21);
-        int presentYear = 2020;
-        if (presentYear % 4 == 0 && presentYear % 100 != 0) {
+        int presentYear = 20;
+        if (presentYear % 4 == 0 && presentYear % 100 != 0 || presentYear % 400 == 0) {
             System.out.print("Rok jest przestępny");
         } else {
             System.out.print("Rok nie jest przestępny");
